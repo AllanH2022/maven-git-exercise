5084b4efb16630359621ba1da3b4642aea69d72d
AllanH <22050073@myrp.edu.sg>
2022-08-18 13:45:23 -0700

    Initial Commit

---
+++ maven1.eclipse/src/main/java/com/sddevops/maven1/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.maven1.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
